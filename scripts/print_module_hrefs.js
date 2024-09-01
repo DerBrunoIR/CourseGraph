968f0bff2d321587dd563d4ade77eb9bee682f17
Bruno Rothschild <bruno.iganz.rothschild@gmail.com>
2024-09-01 14:32:51 +0200

    edit comments

--- scripts/print_module_hrefs.js
+++ scripts/print_module_hrefs.js
@@ -1,12 +1,15 @@
 // script for extracting unique module hrefs from 'https://moseskonto.tu-berlin.de/moses/modultransfersystem/studiengaenge/anzeigen.html?studiengang=179&mkg=24541&semester=73' via a semi automated workflow.
 
 
+// IMPORTANT: It is intended that the following class is pasted into dev tools console.
+// IMPORTANT: Further bugs can arise if the css selectors are outdated.
+
 class Extractor {
 	constructor() {
 		this.hrefs = new Set();
 	}
 
-	// adds all links from individual moses module hrefs of currently listed modules
+	// adds all links from the currently selected program course section
 	extract_links() {
 		let modules_elem = document.querySelector("#j_idt43\\:studiengangsbereich");
 		let module_elems = Array.from(
@@ -16,12 +19,10 @@ class Extractor {
 		console.log('totally extracted links:' + this.hrefs.size);
 	}	
 
-	// returns a list of module hrefs without duplicates
+	// returns a list of unique module hrefs 
 	get_hrefs() {
 		return [...this.hrefs];
 	}
 }
 
 
-/* 
-*/
